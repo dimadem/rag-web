1afeae02478a27d458885e27e4d8e6b06e55f499
Dima Dem <iamdimadem@gmail.com>
2024-06-09 16:13:54 +0000

    add: wikipedia_service

--- backend/app/services/rag_service.py
+++ backend/app/services/rag_service.py
@@ -3,7 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import os
 from app.services.search_service import search_and_parse
-from app.services.buffer.utils import clear_buffer
+from app.services.wikipedia_service import search_wiki
 
 chat_model = ChatOpenAI(
     model="gpt-4o",
@@ -13,7 +13,7 @@ chat_model = ChatOpenAI(
 )
 
 rag_prompt = ChatPromptTemplate.from_messages([
-    ("system", 'You are a helpful assistant. Use the following context when responding:\n\n{context}.'),
+    ("system", 'You are a helpful assistant from Wikipedia. Use the only following context when responding:\n\n{context}. If there no context, just tell that you dont know.'),
     ("user", "{question}")
 ])
 
@@ -22,10 +22,10 @@ rag_chain = rag_prompt | chat_model | StrOutputParser()
 
 
 def process_question(question, context):
-    data_from_web = search_and_parse(question)
+    data = search_wiki(question)
     if context is None:
-        context = data_from_web
-        print("context->>>> ", context)
+        context = data
+        print(f"context->\n", context)
     response = rag_chain.invoke({
         "question": question,
         "context": context
--- backend/app/services/wikipedia_service.py
+++ backend/app/services/wikipedia_service.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from app.services.buffer.utils import write_prompt, write_link, write_text, load_buffer
-import time
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import time
+
 
 def Firefox():
     service = Service()
@@ -22,42 +22,28 @@ def Firefox():
     driver = webdriver.Firefox(service=service, options=options)
     return driver
 
-def search_web(query):
+def search_wiki(query):
     driver = Firefox()
     driver.get("https://en.wikipedia.org/wiki/Main_Page")
-    print("page loaded")
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((
             By.NAME, "search"
             ))).send_keys(query, Keys.RETURN)
+    time.sleep(1)
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "content"))
     )
     content = BeautifulSoup(driver.page_source, 'html.parser')
     article = content.select_one(".mw-parser-output")
+    print(f"article\n", article)
+    if article is None:
+        article_text = "There are no articles on Wikipedia with this title."
+        return article_text
     article_text = ''
     if article:
         paragraphs = article.find_all('p')
         for paragraph in paragraphs:
             article_text += paragraph.text
-
-    print(article_text)
     driver.quit()
-
-def parse_text(query, links):
-    for link in links:
-        driver = Firefox()
-        driver.get(link)
-        time.sleep(1)  
-        body_content = driver.find_element(By.TAG_NAME, "body").text[:400]
-        write_text(prompt=query, text=body_content)
-        driver.quit()
-
-def search_and_parse(query):
-    write_prompt(query)
-    search_web(query)
-    buffer = load_buffer()
-    links = buffer[query]['links']
-    parse_text(query, links)
-    buffer = load_buffer()
-    return buffer[query]['texts']
+    print(f"article\n", article_text)
+    return article_text
